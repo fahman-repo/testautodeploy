ca069f78ad2311f26389bc6d76fea6c903e8049e
fahman-repo <fahman.agoes@gmail.com>
2023-03-07 16:25:33 +0700

    first commit

---
+++ src/main/java/com/demo/testautodeploy/TestautodeployApplication.java
@@ -0,0 +1,13 @@
+package com.demo.testautodeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestautodeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestautodeployApplication.class, args);
+	}
+
+}
